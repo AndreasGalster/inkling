d0cd905dd327ddbc687d0379e5f169e124ba8fe2
Andreas Galster <andreas@product.cafe>
2019-02-09 15:58:11 +0100

    remove export of everything

--- dist/inkling-lit.js
+++ dist/inkling-lit.js
@@ -5,9 +5,9 @@ export { inkColors } from "./ink-colors-lit.js";
 export { inkTypography } from "./ink-typography-lit.js";
 export { inkLayoutHelpers } from "./ink-layout-helpers-lit.js";
 
-import { html } from "lit-html/lib/lit-extended.js";
+// import { html } from "lit-html/lib/lit-extended.js";
 
-export const inkling = html`
-  ${inkVariables} ${inkReset} ${inkFlexGrid} ${inkColors} ${inkTypography}
-  ${inkLayoutHelpers}
-`;
+// export const inkling = html`
+//   ${inkVariables} ${inkReset} ${inkFlexGrid} ${inkColors} ${inkTypography}
+//   ${inkLayoutHelpers}
+// `;
